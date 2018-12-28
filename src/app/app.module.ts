c208fc63a5b898a651eba33030f9c4fd8c05a3d1
Talent Malaba <malabatalent@gmail.com>
2018-12-28 11:25:41 +0800

    clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ScrollingModule } from '@angular/cdk/scrolling';
-import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +12,6 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     ScrollingModule,
-    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
